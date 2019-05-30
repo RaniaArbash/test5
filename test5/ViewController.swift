b6b3a932e2ae75f54ae46592ee946b18c2b12fd2
Rania <raniaarbash@hotmail.com>
2019-05-30 09:46:18 -0400

    design the table and add protocols

--- test5/ViewController.swift
+++ test5/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
